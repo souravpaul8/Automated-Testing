a02ec2c6c429674f58233517708d81b29c9067e7
Sourav <souravpaul@protonmail.com>
2025-07-08 23:16:15 +0530

    Added POST method to test

--- lal_api_testing/api/views.py
+++ lal_api_testing/api/views.py
@@ -1,9 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Person
+from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def get_names(request):
-    names = Person.objects.all().values_list("name", flat=True)
-    return Response({"people": list(names)})
+    if request.method == 'GET':
+        names = Person.objects.all().values_list("name", flat=True)
+        return Response({"people": list(names)})
+
+    elif request.method == 'POST':
+        name = request.data.get("name")
+        if not name:
+            return Response({"error": "Name is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        Person.objects.create(name=name)
+        return Response({"message": f"Person '{name}' added successfully."}, status=status.HTTP_201_CREATED)
 
